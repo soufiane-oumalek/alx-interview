a5b469a1d4080aac7615f568eb40ce2a8b418bc1
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-12 02:12:53 +0100

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
+'''A module for working with lockboxes.
+'''
 
 
 def canUnlockAll(boxes):
-    """function determines if all the boxes can be opened."""
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    '''
     n = len(boxes)
-    visited = set()
-
-    def dfs(box_index):
-        if box_index in visited or box_index >= n or box_index < 0:
-            return
-
-        visited.add(box_index)
-
-        for key in boxes[box_index]:
-            dfs(key)
-
-    dfs(0)
-    """return len visited"""
-    return len(visited) == n
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)

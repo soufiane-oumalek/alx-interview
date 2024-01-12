2c6376be948c5c9197504793005d2dfe370fadf5
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-12 02:31:05 +0100

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 
+from collections import deque
+
 
 def canUnlockAll(boxes):
     """function determines if all the boxes can be opened."""
+    n = len(boxes)
+    visited = set()
+    queue = deque([0])
 
-    total_boxes = len(boxes)
-    opened_boxes = {0}
-    keys_to_check = set(boxes[0])
+    while queue:
+        current_box = queue.popleft()
 
-    while keys_to_check:
-        current_key = keys_to_check.pop()
-        if 0 < current_key < total_boxes and current_key not in opened_boxes:
-            opened_boxes.add(current_key)
-            keys_to_check.update(boxes[current_key])
-    """ return opened_boxes"""
-    return len(opened_boxes) == total_boxes
+        if current_box in visited:
+            continue
 
+        visited.add(current_box)
+        queue.extend(boxes[current_box])
+
+    return len(visited) == n

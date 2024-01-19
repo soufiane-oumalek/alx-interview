0b49af07eb487f3de162611b494c571a63dc85d4
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-19 01:27:53 +0100

    minimum_operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,20 +3,20 @@
 
 
 def minOperations(n):
-  '''method that calculates the fewest number of operations'''
+    '''method that calculates the fewest number of operations'''
 
-  if not isinstance(n, int) or n <= 0:
-      return 0
+    if not isinstance(n, int) or n <= 0:
+        return 0
 
-  operations = 0
-  current_length = 1
-  clipboard = 0
+    operations = 0
+    current_length = 1
+    clipboard = 0
 
-  while current_length < n:
-      if n % current_length == 0:
-          clipboard = current_length
-          operations += 2
-      current_length += clipboard
-      operations += 1
+    while current_length < n:
+        if n % current_length == 0:
+            clipboard = current_length
+            operations += 2
+        current_length += clipboard
+        operations += 1
 
-  return operations - 1
+    return operations - 1
